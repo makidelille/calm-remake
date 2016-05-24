caed9e6ff53844696506e972daed3a111fda1b82
makidelille <makidelille@github.com>
2016-05-24 22:19:42 +0200

    made the club page added smooth scrolling TODO implements footers TODO footer sync TODO fix xs version TODO the other pages

--- new/js/index.js
+++ new/js/index.js
@@ -2,6 +2,13 @@ String.prototype.contains = function(it) {
     return this.indexOf(it) != -1;
 };
 
+Array.prototype.contains = function(it) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === it) return true;
+    }
+    return false;
+}
+
 // window.onscroll = function(){
 //   var toggleBarScroll = document.getElementById('navbar').scrollHeight/2;
 //   if(document.body.scrollTop > toggleBarScroll && !document.getElementById('navbar').className.contains('navbar-fixed-top')){
@@ -22,6 +29,33 @@ $(document).ready(function() {
         }
     });
 
+    $(".footer a, .dropdown-menu a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
 
 
+            /**smooth scrolling**/
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            // Store hash (#)
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+
+            /*navbar retracting*/
+            console.log($(this));
+            if ($(this).parents('#footer_xs').length > 0) {
+                $(".footer button.navbar-toggle").click();
+                return;
+            } else if ($(this).parents($('#navbar_xs')).length > 0) {
+                $(".header button.navbar-toggle").click();
+                return;
+            }
+        }
+    });
 });
